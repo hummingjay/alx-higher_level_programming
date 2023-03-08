5ae342cd192ce61343233057347e398cab82cbaf
hummingjay <jays7mill@gmail.com>
2023-03-08 22:05:18 +0300

    padded with 0

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for num in range(00, 100):
     if num < 99:
-        print("{}".format(int(num)), end = ", ")
+        # to pad a digit less than 2 digits with o before is with :0>2
+        print("{:0>2}".format(int(num)), end=", ")
     else:
         print("{}".format(int(num)))
