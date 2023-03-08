96dea8a844774e8bb9663ebd9be21d4cbc6d929c
hummingjay <jays7mill@gmail.com>
2023-03-08 23:43:00 +0300

    better my previous error

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     for ch in range(97, 123):
-        if c != chr:
-            return False
-        elif c == chr(ch):
+        if c == chr(ch):
             return True
         else:
             return False
