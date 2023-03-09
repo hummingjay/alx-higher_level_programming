6b95687b5ecaf67a8ba80789f99eb5bb6d7183b3
hummingjay <jays7mill@gmail.com>
2023-03-10 01:44:02 +0300

    checking if run as main not import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 # import the function from add_0.py
-import add from add_0 
+from add_0 import add
 
 # check if module is run as main by using __name__
-if __main__ == "__main__":
+if __name__ == "__main__":
     a = 1
     b = 2
     # call the function add
     result = add(a, b)
-    print("{} + {} = result".format(<a, b, result))
+    print("{} + {} = {}".format(<a, b, result))
