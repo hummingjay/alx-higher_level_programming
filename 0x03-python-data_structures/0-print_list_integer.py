b95e43b01890fccebda2911d5ad0fc80b0f92907
hummingjay <jays7mill@gmail.com>
2023-03-13 12:46:04 +0300

    SImpler and works

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if len(my_list) != 0:
-        print("{}".format(my_list.popleft))
+    for integer in my_list:
+        # {:d} specifies it's the int followed by new line
+        print("{:d}".format(integer))
