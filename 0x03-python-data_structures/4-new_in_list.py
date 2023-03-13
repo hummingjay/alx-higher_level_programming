e0efc009958d629dfb8ae5b5ae40c13dcded6182
hummingjay <jays7mill@gmail.com>
2023-03-13 15:24:53 +0300

    forgort the underscore

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,6 +7,6 @@ def new_in_list(my_list, idx, element):
         if idx < 0 or idx >= len(my_list):
             return my_list
         else:
-            new = mylist.copy()
+            new = my_list.copy()
             new[idx] = element
             return new
