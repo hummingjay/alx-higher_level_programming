d10337bde0037b83bab1efbf29f39a56753b43a2
hummingjay <jays7mill@gmail.com>
2023-03-14 09:51:26 +0300

    correct order

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None or len(sentence) == 0:
-        return (None, 0)
+        return (0, None)
     else:
-        return(sentence[0], len(sentence))
+        return(len(sentence), sentence[0])
