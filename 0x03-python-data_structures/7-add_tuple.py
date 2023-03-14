74d30772dab5ecf9ba6ac5bdf71b31358d52f46a
hummingjay <jays7mill@gmail.com>
2023-03-14 17:23:20 +0300

    it needed some space

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,7 +4,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     # needs to check how many 0s to add to the tuple if < 2
     if len(tuple_a) < 2:
         tuple_a += extend * (2 - len(tuple_a))
-    if len(tuple_b) <2:
+    if len(tuple_b) < 2:
         tuple_b += extend * (2 - len(tuple_b))
     # use only the first 2 integers of the tuple
     a, b = tuple_a[:2]
