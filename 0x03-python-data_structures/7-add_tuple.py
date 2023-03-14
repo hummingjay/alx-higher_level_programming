5138601a3ffe56863a8dba302e0e090f78c8e3a3
hummingjay <jays7mill@gmail.com>
2023-03-14 08:29:21 +0300

    7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,13 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    extend = (0,)
+    # needs to check how many 0s to add to the tuple if < 2
+    if len(tuple_a) < 2:
+        tuple_a += extend * (2 - len(tuple_a))
+    if len(tuple_b) <2:
+        tuple_b += extend * (2 - len(tuple_b))
+    # use only the first 2 integers of the tuple
+    a, b = tuple_a[:2]
+    c, d = tuple_b[:2]
+
+    # calculate the sum
+    return (a + c, b + d)
