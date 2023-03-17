de0a9fec6e30be3bee5f11943f7ff3ad53f883d8
hummingjay <jays7mill@gmail.com>
2023-03-17 22:05:04 +0300

    stripping

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if key.strip() in a_dictionary:
-        del a_dictionary[key]
+        del a_dictionary[key.strip()]
     return a_dictionary
