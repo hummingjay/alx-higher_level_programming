96472549a40e664871ef61057e54e64637cd2436
hummingjay <jays7mill@gmail.com>
2023-03-17 13:18:40 +0300

    new list right time

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,12 +2,13 @@
 def search_replace(my_list, search, replace):
     if not my_list:
         return None
+    # make new list before traversing
+    new_list = []
     # traverse list
     for i in my_list:
-        # make new list
-        new_list = []
         # now fill list with append replacing where needed
         if i == search:
             new_list.append(replace)
         else:
             new_list.append(i)
+    return (new_list)
