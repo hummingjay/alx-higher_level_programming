f650a2fde0d1fa52edba3612587da3560565655e
hummingjay <jays7mill@gmail.com>
2023-03-17 23:10:55 +0300

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,33 @@
+def roman_to_int(roman_string):
+    roman_string = roman_string.upper()
+    if roman_string not str:
+        return 0
+    if roman_string is None:
+        return 0
+
+    roman_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+            }
+    result = 0
+    prev_val = 0
+
+    for num in roman_string:
+        curr_val = roman_dict.get(num, 0)
+
+        if curr_val == 0:
+            return 0
+
+        if curr_val > prev_val:
+            result += curr_val - 2 * pre_val
+        else:
+            result += curr_val
+
+        prev_val = curr_val
+
+    return result
