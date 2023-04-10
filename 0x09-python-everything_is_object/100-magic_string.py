8e0ea3a5574a997f0cae2dd81984442294c8257a
hummingjay <jays7mill@gmail.com>
2023-04-10 12:30:35 +0300

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    result = "\n".join([", ".join(["BestSchool"] * (i + 1))for i in range(10)])
+    result = "\n".join([", ".join(["BestSchool"] * (i + 1)) + "$" for i in range(10)])
     return result
