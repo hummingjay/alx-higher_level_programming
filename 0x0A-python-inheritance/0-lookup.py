77fcc638151906724e235d8599f7e6609f30573f
hummingjay <jays7mill@gmail.com>
2023-04-11 22:02:05 +0300

    Docummentation?

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Deefines an attribute object lookup function"""
+
+
 def lookup(obj):
     """Function that returns the list of available attributes and methods"""
     attr_and_methods = dir(obj)
