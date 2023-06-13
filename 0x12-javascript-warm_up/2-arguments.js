1c77561ddd85bfa795181208e008340b18579857
hummingjay <jays7mill@gmail.com>
2023-06-13 06:18:45 +0300

    2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (arguments.length === 2){
+	console.log('No argument');
+}
+else if (arguments.length === 3){
+	console.log('Argument found');
+}
+else{
+	console.log('Arguments found');
+}
