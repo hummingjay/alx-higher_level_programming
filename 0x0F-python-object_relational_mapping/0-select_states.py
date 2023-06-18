49078d6b93ef9707eab8cd07e0e599805b45be26
hummingjay <jays7mill@gmail.com>
2023-06-18 23:23:49 +0300

    0-select_states.py again

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,39 +3,30 @@
 This is a python script that lists all states from
 the database hbtn_0e_0_usa.
 '''
-import MySQLdb
-
-# connecting to a MySQL database
-db = MySQLdb.connect(host="localhost", user=username,
-                     passwd=password, db=database, port=3306)
-
-# creating cursor
-cur = db.cursor()
-
-# script to execute listing of states
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-# fetching all at once
-rows = cur.fetchall()
-
-# print out data
-for row in rows:
-    print(row)
-
-# close the open cursor
-cur.close()
-
-# close open database
-db.close()
-
-# program entry point to avoid running if imported
+# program entry to avoid running if imported
 if __name__ == "__main__":
     import sys
+    import MySQLdb
 
-    # retrieve command line inputs
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    
-    # call the function to list states
-    list_states(username, password, database)
+    # connecting to a MySQL database
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
+    # creating cursor
+    cur = db.cursor()
+
+    # script to execute listing of states
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # fetching all at once
+    rows = cur.fetchall()
+
+    # print out data
+    for row in rows:
+        print(row)
+
+    # close the open cursor
+    cur.close()
+
+    # close open database
+    db.close()
