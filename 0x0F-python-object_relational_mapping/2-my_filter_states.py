ef743be12519ae51f970e2c8df10d13ab8332700
hummingjay <jays7mill@gmail.com>
2023-06-19 06:23:21 +0300

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,15 +6,21 @@ the states table of hbtn_0e_0_usa where name matches arg
 import sys
 import MySQLdb
 
-if __name__ == "__main":
+if __name__ == "__main__":
+    if len(sys.argv) < 5:
+        print("Usage: python3 script.py username password
+              database name")
+        sys.exit(1)
+
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
 
     namearg = argv[4]
 
     cur = db.cursor()
+    # execute takes two arguments first sql string then sub
     cur.execute("SELECT * FROM states WHERE name LIKE
-                   '{namearg}' ORDER BY id ASC")
+                %s ORDER BY id ASC", (namearg))
 
     rows = cur.fetchall()
 
