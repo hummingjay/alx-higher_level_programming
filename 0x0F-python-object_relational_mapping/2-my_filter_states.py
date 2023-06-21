802af5e911754ccec2ccfefc4b896b2b14a6b22b
hummingjay <jays7mill@gmail.com>
2023-06-21 15:38:45 +0300

    2-my_filter_states.py checker why??

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,14 +5,6 @@ import sys
 
 
 if __name__ == "__main__":
-    """
-    Main program running
-    """
-    if len(sys.argv) < 5:
-        print("Usage: python3 script.py username password database
-               name")
-        sys.exit(1)
-
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
