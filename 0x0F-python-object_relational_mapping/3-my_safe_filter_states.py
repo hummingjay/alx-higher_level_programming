ef62c16e10a3684c5c44dd708742504430178b73
hummingjay <jays7mill@gmail.com>
2023-06-21 18:03:30 +0300

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,7 +9,7 @@ import MySQLdb
 import sys
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     # connect to db
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
