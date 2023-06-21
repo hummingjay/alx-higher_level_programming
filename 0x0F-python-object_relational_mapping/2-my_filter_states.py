697ff61f2391ab05aa15a5b40add6b718f1081a8
hummingjay <jays7mill@gmail.com>
2023-06-21 15:35:36 +0300

    2-my_filter_states.py checker why??

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,11 +5,16 @@ import sys
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 5:
+        print("Usage: python3 script.py username password database
+               name")
+        sys.exit(1)
+
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}' 
+                 ORDER BY id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
