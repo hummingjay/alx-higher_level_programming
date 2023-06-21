580b6ddced5bede4e3bccfd9fa0ea7545e5a472c
hummingjay <jays7mill@gmail.com>
2023-06-21 09:50:10 +0300

    2-my_filter_states.py bloody checker

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,32 +1,17 @@
 #!/usr/bin/python3
-'''
-This is a script that takes in an arg and displays all values
-in states table of hbtn_0e_0_usa where name matches arg
-'''
-import sys
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
 
-# program entry
+
 if __name__ == "__main__":
-    if len(sys.argv) < 5:
-        print("Usage: python3 script.py username password
-              database name")
-        sys.exit(1)
-
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-
-    namearg = sys.argv[4]
-
     cur = db.cursor()
-    # execute takes two arguments first sql string then sub
-    cur.execute("SELECT * FROM states WHERE name LIKE '{}'
-                ORDER BY id ASC".format(namearg))
-
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
     rows = cur.fetchall()
-
     for row in rows:
         print(row)
-
     cur.close()
     db.close()
